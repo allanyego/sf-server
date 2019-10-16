e6af592115f2e09c2421e02dbd2eefbce71e650a
yegow <allanyego05@gmail.com>
2019-10-16 20:46:32 +0300

    added stubs for / and * routes

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
+app.use('/', function(req, res, next) {
+	res.status(200).json({
+		msg: "Hello world"
+	});
+});
 app.use('/users', users);
 app.use('/bids', bids);
 app.use('/clients', clients);
@@ -31,6 +36,11 @@ app.use('/contracts', contracts);
 app.use('/reviews', reviews);
 app.use('/talents', talents);
 app.use('/threads', threads);
+app.use('*', function(req, res, next) {
+	res.status(404).json({
+		msg: "Not Found"
+	});
+})
 
 app.use(function (err, req, res, next) {
 	console.error(err);
